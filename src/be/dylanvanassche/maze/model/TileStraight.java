3d4a666133faaff85b1d8fb1533027e72c84f06a
Dylan Van Assche <dylan.van.assche@protonmail.com>
2017-11-29 13:41:48 +0100

    Added Java source files

---
+++ src/be/dylanvanassche/maze/model/TileStraight.java
@@ -0,0 +1,17 @@
+// Dylan Van Assche - 3 ABA EI
+
+package be.dylanvanassche.maze.model;
+
+import be.dylanvanassche.maze.model.Tile;
+
+public class TileStraight extends Tile {
+	final private Square[][] squares = {
+			{new Square(SquareType.WALL), new Square(SquareType.WALL), new Square(SquareType.WALL)}, 
+			{new Square(SquareType.FREE), new Square(SquareType.FREE), new Square(SquareType.FREE)},
+			{new Square(SquareType.WALL), new Square(SquareType.WALL), new Square(SquareType.WALL)}
+			};
+
+	public TileStraight() {
+		super.setSquares(squares);
+	}
+}
